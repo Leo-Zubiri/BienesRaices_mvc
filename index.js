60dff193679e0ab63bd7ea9336c1bb0cb91f29fb
Leo-Zubiri <leo.zubiri@outlook.com>
2023-04-13 20:38:59 -0600

    Add express basic server config

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hola mundo');
+const express = require('express');
+
+// Crear la app
+const app = express();
+
+// Routing
+app.get('/',function(req,res){
+    res.send('Hola mundo desde Express');
+});
+
+// Definir puerto
+const port = 3000;
+
+app.listen(port,() => { 
+    console.log(`Servidor funcionando en el puerto ${port}`);
+});
