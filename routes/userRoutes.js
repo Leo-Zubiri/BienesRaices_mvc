45d0f0605b120bf7229efab43d440a0df995ec98
Leo-Zubiri <leo.zubiri@outlook.com>
2023-04-14 23:12:15 -0600

    add data to the view

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,10 @@ import express from "express";
 const router = express.Router();
 
 router.get('/login',function(req,res){
-    res.render('auth/login');
+    res.render('auth/login',{
+        autenticado: true,
+        user: 'Leo Zubiri'
+    });
 });
 
 export default router;
