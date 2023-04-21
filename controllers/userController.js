d9d0fa17501f509458606ac9953da54a6240af51
Leo-Zubiri <leo.zubiri@outlook.com>
2023-04-20 21:59:50 -0600

    Add validation to Register form

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,13 +22,26 @@ const formularioOlvidePassword = (req,res)=>{
 
 const registrar = async (req,res) => { 
     // Validation
-    await check('name').notEmpty.withMessage('Nombre no puede ir vacío').run(req)
+    await check('name').notEmpty().withMessage('Nombre no puede ir vacío').run(req)
+    await check('email').isEmail().withMessage('No es un email válido').run(req)
+    await check('password').isLength({min: 6}).withMessage('Password debe de ser de al menos 6 caracteres').run(req)
+    await check('password_confirmation').equals('password').withMessage('Los Passwords no coinciden').run(req)
 
-    let resultado = validationResult(req)
+    let errores = validationResult(req)
 
-    const usuario = await Usuario.create(req.body);
+    // Verificar que el resultado esté vacío
+    if(errores.isEmpty()){
+        const usuario = await Usuario.create(req.body);
+        res.json(usuario)
+    }else{
+        res.render('auth/register',{
+            pagina: 'Crear Cuenta',
+            errores: errores.array()
+        });
+    }
+        
 
-    res.json(usuario)
+
 }
 
 export {
